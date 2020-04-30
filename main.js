7632713d9a871a144b67f6f7da17b44bb404749c
Manav Misra <manavm@visionwebsoft.com>
2020-04-30 11:46:38 -0500

    :tada: Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// const displayedImage = document.querySelector('.displayed-img')
+const thumbBar = document.querySelector('.thumb-bar')
+
+// const btn = document.querySelector('button')
+// const overlay = document.querySelector('.overlay')
+
+/**
+ * Loop through images - assume only 5 images - 'for' 1 to 5 - create markup
+ *
+ * TODO:
+ * Create an '<img>' - https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+ *
+ * TODO:
+ * Set 'src' taking advantage of the fact that they are 'pic1', 'pic2';
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/
+ *
+ * After looping...
+ *  TODO: Append to '.thumb-bar-flex' - https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+ */
+
+/* Darken/Lighten button */
